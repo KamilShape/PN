843a64cc1bd78a7087164d7f026b5035da6037d4
Kamil <kamilshape@gmail.com>
2020-03-28 16:27:56 +0100

    animations

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const hamburgerIcon = document.querySelector('.menu__hamburger_icon')
 const menuContainer = document.querySelector('.menu__container2')
-// console.log(menuBars)
+const topBar = document.querySelector('.menu__hamburger_bar_top')
+const middleBar = document.querySelector('.menu__hamburger_bar_middle')
+const bottomBar = document.querySelector('.menu__hamburger_bar_bottom')
+console.log(hamburgerIcon)
 hamburgerIcon.addEventListener('click', function () {
     menuContainer.classList.toggle('menu__container2--active')
+    topBar.classList.toggle('menu__hamburger_bar_top--active')
+    bottomBar.classList.toggle('menu__hamburger_bar_bottom--active')
+    middleBar.classList.toggle('menu__hamburger_bar--active')
+
 })
